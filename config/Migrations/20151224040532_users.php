6effe6e940e88bbb86404374335979164ee3739a
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2015-12-24 04:11:47 +0000

    ひとまずのマイグレーションファイル作成

---
+++ config/Migrations/20151224040532_users.php
@@ -0,0 +1,26 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Users extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('users');
+        $table->addColumn('email', 'string', ['limit' => 50])
+              ->addColumn('password', 'string', ['limit' => 100])
+              ->addColumn('registered', 'datetime')
+              ->addColumn('exited', 'datetime')
+              ->addColumn('is_deleted', 'boolean', ['default' => false])
+              ->addColumn('created', 'datetime')
+              ->addColumn('modified', 'datetime')
+              ->addIndex(['email'], ['unique' => true])
+              ->create();
+    }
+}
