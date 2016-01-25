f6a667b2e65126d7ee2497226993ffc3a0617e21
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2016-01-25 12:31:01 +0000

    審査コミュニティに居住地各２つと名称追加

--- config/Migrations/20160125122716_AddAddressToReviewCommunities.php
+++ config/Migrations/20160125122716_AddAddressToReviewCommunities.php
@@ -19,7 +19,7 @@ class AddAddressToReviewCommunities extends AbstractMigration
         $table->addColumn('hometown_country_id', 'integer', ['limit' => 11, 'after' => 'city_id'])
               ->addColumn('hometown_ken_id', 'integer', ['limit' => 11, 'after' => 'hometown_country_id']) // ad_address.ken_id 都道府県
               ->addColumn('hometown_city_id', 'integer', ['limit' => 11, 'after' => 'hometown_ken_id']); // ad_address.city_id 市町村
-        $table->addColumn('name', 'string', ['limit' => 100, 'after' => 'hometown_city_id']);
+        $table->addColumn('name', 'string', ['limit' => 100, 'after' => 'community_status_id']);
 
         $table->update();
     }
