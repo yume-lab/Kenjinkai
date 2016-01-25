21dc9d77f22a50719f0a098129ab8b14adc717ab
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2016-01-25 07:42:22 +0000

    管理者マスタ追加

---
+++ config/Migrations/20160125073750_admins.php
@@ -0,0 +1,25 @@
+<?php
+use Migrations\AbstractMigration;
+
+class Admins extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-change-method
+     * @return void
+     */
+    public function change()
+    {
+        $table = $this->table('admins');
+        $table->addColumn('email', 'string', ['limit' => 50])
+              ->addColumn('password', 'string', ['limit' => 255])
+              ->addColumn('registered', 'datetime')
+              ->addColumn('is_deleted', 'boolean', ['default' => false])
+              ->addColumn('created', 'datetime')
+              ->addColumn('modified', 'datetime')
+              ->addIndex(['email'], ['unique' => true])
+              ->create();
+    }
+}
---
+++ tests/Fixture/AdminsFixture.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * AdminsFixture
+ *
+ */
+class AdminsFixture extends TestFixture
+{
+
+    /**
+     * Fields
+     *
+     * @var array
+     */
+    // @codingStandardsIgnoreStart
+    public $fields = [
+        'id' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => null, 'comment' => '', 'autoIncrement' => true, 'precision' => null],
+        'email' => ['type' => 'string', 'length' => 50, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+        'password' => ['type' => 'string', 'length' => 255, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null, 'fixed' => null],
+        'registered' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'is_deleted' => ['type' => 'boolean', 'length' => null, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null],
+        'created' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        'modified' => ['type' => 'datetime', 'length' => null, 'null' => false, 'default' => null, 'comment' => '', 'precision' => null],
+        '_constraints' => [
+            'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
+            'email' => ['type' => 'unique', 'columns' => ['email'], 'length' => []],
+        ],
+        '_options' => [
+            'engine' => 'InnoDB',
+            'collation' => 'utf8_general_ci'
+        ],
+    ];
+    // @codingStandardsIgnoreEnd
+
+    /**
+     * Records
+     *
+     * @var array
+     */
+    public $records = [
+        [
+            'id' => 1,
+            'email' => 'Lorem ipsum dolor sit amet',
+            'password' => 'Lorem ipsum dolor sit amet',
+            'registered' => '2016-01-25 07:39:13',
+            'is_deleted' => 1,
+            'created' => '2016-01-25 07:39:13',
+            'modified' => '2016-01-25 07:39:13'
+        ],
+    ];
+}
