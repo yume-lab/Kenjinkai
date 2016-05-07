b2fd5bca9230c4b29e0bd71ebf68c94c37b5ac0a
Ippei Fukamatsu <ippei.fukamatsu@yume-lab.co.jp>
2016-05-07 14:00:31 +0000

    pages controllerの認証切った

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -15,6 +15,11 @@ use Cake\View\Exception\MissingTemplateException;
 class PagesController extends AppController
 {
 
+    public function initialize() {
+        parent::initialize();
+        $this->Auth->allow(['display']);
+    }
+
     /**
      * Displays a view
      *
